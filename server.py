a9d40457fafe7751ccaadaab438651a0b277bfb5
Keerthansimha <124924804+Keerthansimha@users.noreply.github.com>
2025-04-16 18:11:09 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,17 @@
 # server.py
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template_string
 import json
 
 app = Flask(__name__)
 
 @app.route('/')
+def home():
+    return render_template_string("""
+        <h1> Welcome to the web sccccrrrraaapeeer!! </h1>
+        <p> Goooo to <a href = "/data"> /data </a> to view the scrapeed content. </p>
+        """)
+
+@app.route('/data')
 def index():
     with open('scraped_data.json') as f:
         data = json.load(f)
